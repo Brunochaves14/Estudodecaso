b591889f3aadf62bb1e4813c6b51aaa554e7304e
Brunochaves14 <91095039+Brunochaves14@users.noreply.github.com>
2021-11-02 11:09:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document
+.querySelector("button.fat")
+.addEventListener("click", function(params){
+    document
+    .querySelector("#contact")
+    .classList
+    .toggle("hide")
+})
+
+document
+.querySelector("button.but")
+.addEventListener("click", function(params){
+    document
+    .querySelector("#rodape")
+    .classList
+    .toggle("hide")
+})
+
+
+
+
